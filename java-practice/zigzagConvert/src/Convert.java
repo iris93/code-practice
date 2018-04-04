300e90d1cc6ba34889411a3008d40e9d3de57f37
iris66 <xujj666@126.com>
2018-04-04 15:52:20 +0800

    完成java版本的zigzag，体会到StringBuffer相对于String的区别

--- java-practice/zigzagConvert/src/Convert.java
+++ java-practice/zigzagConvert/src/Convert.java
@@ -1,4 +1,6 @@
 public class Convert {
+//    和 String 类不同的是，StringBuffer 和 StringBuilder 类的对象能够被多次的修改，并且不产生新的未使用对象.
+//    执行效率从17%飞跃到95.05%
 
     public static String convert(String s,int numRows){
         int totalLen;
@@ -6,13 +8,33 @@ public class Convert {
         if (totalLen < 2 || numRows==1) {
             return s;
         }
-        int itemLen = 2*numRows-2
+//        String result = new String();
+//        用StringBuffer替换
+        StringBuffer result = new StringBuffer();
+        int itemLen = 2*numRows-2;
         int numColunm;
-        numColunm = totalLen/itemLen+1
+        numColunm = totalLen/itemLen+1;
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numColunm;j++){
+                int item1Index = itemLen*j+i;
+                if (item1Index < totalLen){
+//                    result += s.charAt(item1Index);
+                    result.append(s.charAt(item1Index));
+                    if (i>0 && i<numRows-1){
+                        int item2Index = item1Index+itemLen-2*i;
+                        if (item2Index<totalLen){
+//                            result += s.charAt(item2Index);
+                            result.append(s.charAt(item2Index));
+                        }
+                    }
+                }
+            }
+        }
+        return result.toString();
     }
     public static void main(String args[]){
-        String s = "Thisistest";
-        String result = convert(s,1);
+        String s = "PAYPALISHIRING";
+        String result = convert(s,3);
         System.out.println(result);
     }
 }
